59714e89dec64b60eb7869be8da3d8a39dd4f613
racine14creator <bisimwagrace596@gmail.com>
2024-05-23 21:38:23 +0200

    inti project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,9 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'my-angular-app';
+  title: string = 'Bisimwa WebSite';
+  myArray: string[] = ['ReactJs', 'Next.js'];
 }
